0745562a78abffa71ceae4e576dd96eda8994877
henry <henry@iMac-home.local>
2022-02-06 03:27:51 +0900

    back to the ....

--- src/components/CreateTodo/DeleteButton.tsx
+++ src/components/CreateTodo/DeleteButton.tsx
@@ -15,20 +15,7 @@ const DeleteButton = ({ id, category }: any) => {
       [category]: [...copyTodoList],
     });
     localStorage.setItem(category, JSON.stringify(todoListObject[category]));
-
-    // setTodoList((oldToDos) => {
-    //   const copyTodos = [...oldToDos];
-    //   const targetIndex = copyTodos.findIndex((todo) => todo.id === id);
-    //   copyTodos.splice(targetIndex, 1);
-    //   return [...copyTodos];
-    // });
-    // localStorage.setItem("todoList", JSON.stringify(todoList));
   };
   return <button onClick={onDelete}>DEL</button>;
 };
 export default DeleteButton;
-
-// setTodoListObject({
-//   ...todoListObject,
-//   [selectedCategory]: [...Target, newTodo],
-// });
